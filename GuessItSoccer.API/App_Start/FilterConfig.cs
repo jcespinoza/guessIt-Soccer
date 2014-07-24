afc78f11e99418227bf9351131622b23d811051c
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-07-24 06:36:37 -0600

    More files

---
+++ GuessItSoccer.API/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GuessItSoccer.API
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
