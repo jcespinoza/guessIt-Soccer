4514c04425e7a2a8c55902d171f1dc3cb65c329a
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-08-14 18:33:30 -0600

    Added Leagues view
    Also added a local array of teams in leagues in the LeaguesCtrl

--- GuessItSoccer.Presentation/Scripts/controllers.js
+++ GuessItSoccer.Presentation/Scripts/controllers.js
@@ -41,6 +41,57 @@ angular.module('app.controllers', [])
     .controller('LeaguesCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
         $scope.$root.title = 'GuessIt Soccer | Leagues';
         
+        $scope.leagues =[{id: 1, name: "Spanish La Liga"},
+                            {id: 2, name: "English Premier League"},
+                            {id: 3, name: "Italian Serie A"},
+                            {id: 4, name: "German Bundesliga"},
+                            {id: 5, name: "Honduran Liga Nacional"}
+                        ];
+
+        $scope.teams = [
+                        {leagueID: 1, teamID: 1, name: "Real Madrid FC"},
+                        {leagueID: 1, teamID: 2, name: "FC Barcelona"},
+                        {leagueID: 1, teamID: 3, name: "Atletico Madrid"},
+                        {leagueID: 1, teamID: 4, name: "Valencia"},
+                        {leagueID: 1, teamID: 5, name: "Villareal"},
+                        {leagueID: 1, teamID: 6, name: "Sevilla FC"},
+                        {leagueID: 1, teamID: 7, name: "Real Sociedad"},
+                        {leagueID: 1, teamID: 8, name: "Malaga"},
+                        {leagueID: 2, teamID: 1, name: "Manchester United"},
+                        {leagueID: 2, teamID: 2, name: "Chealsea FC"},
+                        {leagueID: 2, teamID: 3, name: "Everton"},
+                        {leagueID: 2, teamID: 4, name: "Arsenal"},
+                        {leagueID: 2, teamID: 5, name: "Manchester City"},
+                        {leagueID: 2, teamID: 6, name: "Liverpool"},
+                        {leagueID: 2, teamID: 7, name: "Tottenham Hotspur"},
+                        {leagueID: 2, teamID: 8, name: "New Castle"},
+                        {leagueID: 3, teamID: 1, name: "Juventus"},
+                        {leagueID: 3, teamID: 2, name: "Roma"},
+                        {leagueID: 3, teamID: 3, name: "AC Milan"},
+                        {leagueID: 3, teamID: 4, name: "Internazionale"},
+                        {leagueID: 3, teamID: 5, name: "Caigliari"},
+                        {leagueID: 3, teamID: 6, name: "Lazio"},
+                        {leagueID: 3, teamID: 7, name: "Palermo"},
+                        {leagueID: 3, teamID: 8, name: "Torino"},
+                        {leagueID: 4, teamID: 1, name: "Bayern München"},
+                        {leagueID: 4, teamID: 2, name: "borussia Dortmund"},
+                        {leagueID: 4, teamID: 3, name: "Schalke 04"},
+                        {leagueID: 4, teamID: 4, name: "Hannover 96"},
+                        {leagueID: 4, teamID: 5, name: "Stuttgart"},
+                        {leagueID: 4, teamID: 6, name: "Wolfsburg"},
+                        {leagueID: 4, teamID: 7, name: "Werder Bremen"},
+                        {leagueID: 4, teamID: 8, name: "Köln"},
+                        {leagueID: 1, teamID: 1, name: "Olimpia"},
+                        {leagueID: 1, teamID: 2, name: "Platense"},
+                        {leagueID: 1, teamID: 3, name: "Marathon"},
+                        {leagueID: 1, teamID: 4, name: "Parrillas One"},
+                        {leagueID: 1, teamID: 5, name: "Real España"},
+                        {leagueID: 1, teamID: 6, name: "Honduras Progreso"},
+                        {leagueID: 1, teamID: 7, name: "Vida"},
+                        {leagueID: 1, teamID: 8, name: "Victoria"}
+                        ];
+        
+
         $scope.$on('$viewContentLoaded', function () {
             $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
         });
