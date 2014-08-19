44c902773d786663b2dbeed0b01da5819a2bbbf1
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-08-18 23:11:01 -0600

    Added some functions in controllers.js

--- GuessItSoccer.Presentation/Scripts/controllers.js
+++ GuessItSoccer.Presentation/Scripts/controllers.js
@@ -59,13 +59,28 @@ angular.module('app.controllers', [])
     .controller('LeaguesCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
         $scope.$root.title = 'GuessIt Soccer | Leagues';
         
+        $scope.isEditing = false;
+        $scope.newLeagueName = "";
+        $scope.updatedName = "";
+
         $scope.leagues =[{id: 1, name: "Spanish La Liga", isEnabled: true},
                             {id: 2, name: "English Premier League", isEnabled: true},
                             {id: 3, name: "Italian Serie A", isEnabled: true},
                             {id: 4, name: "German Bundesliga", isEnabled: true},
                             {id: 5, name: "Honduran Liga Nacional", isEnabled: true}
                         ];
+        
+        $scope.updateLeague = function(league){
+            league.name = $scope.updatedName;
+            //disable planel
+        };
 
+        $scope.addNewTeam = function(){
+            var nid = $scope.leagues[$scope.leagues.length-1].id + 1;
+            $scope.leagues.push(
+                { id: nid, name: $scope.newLeagueName, isEnabled: true };
+            );
+        };
 
         $scope.setLeagueEnabled = function(league, value){
             league.isEnabled = value;
