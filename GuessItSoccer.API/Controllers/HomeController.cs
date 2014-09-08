326443da65518eecaf46fad9fbc94cb69258cfc0
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-09-07 23:51:31 -0600

    Fixed the values and home controller
    All other controllers fail to function

--- GuessItSoccer.API/Controllers/HomeController.cs
+++ GuessItSoccer.API/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Web.Mvc;
+using AttributeRouting.Web.Mvc;
 
 namespace GuessItSoccer.API.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        [AcceptVerbs("GET", "HEAD")]
+        [GET("")]
         public ActionResult Index()
         {
             return View();
--- GuessItSoccer.API/Controllers/ValuesController.cs
+++ GuessItSoccer.API/Controllers/ValuesController.cs
@@ -1,33 +1,49 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using AttributeRouting.Web.Http;
 
 namespace GuessItSoccer.API.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
+        [HttpGet]
+        [AcceptVerbs("GET", "HEAD")]
+        [GET("values")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        [HttpGet]
+        [AcceptVerbs("GET", "HEAD")]
+        [GET("values/{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
+        [HttpPost]
+        [AcceptVerbs("POST", "HEAD")]
+        [POST("values")]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
+        [HttpPut]
+        [AcceptVerbs("PUT", "HEAD")]
+        [POST("values/{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
+        [HttpDelete]
+        [AcceptVerbs("DELETE", "HEAD")]
+        [POST("values/{id}")]
         public void Delete(int id)
         {
         }
