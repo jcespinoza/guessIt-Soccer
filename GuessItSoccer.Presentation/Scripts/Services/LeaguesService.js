5b1649001cbe3f2704c4e086bad51cb12fa3708b
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-09-12 21:01:02 -0600

    Fiexd typo
    ServerService was mispelled

--- GuessItSoccer.Presentation/Scripts/Services/LeaguesService.js
+++ GuessItSoccer.Presentation/Scripts/Services/LeaguesService.js
@@ -1,11 +1,11 @@
 ﻿﻿'use strict';
 angular.module('app.services')
-    .factory('LeaguesService', function ($http, ServerSerive, $cookieStore) {
+    .factory('LeaguesService', function ($http, ServerService, $cookieStore) {
             return {
                 getAvailableLeagues: function (success, error) {
                         $http
                             .get(
-                                ServerSerive.get() + '/leagues/available', {
+                                ServerService.get() + '/leagues/available', {
                                         Authorization: $cookieStore.get('access_token')
                                 })
                         .success(function (response) {
