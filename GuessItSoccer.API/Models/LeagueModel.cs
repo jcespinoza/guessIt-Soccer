97f594e0283eb81f896782c0004cd82f634f8fa4
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-09-18 17:33:48 -0600

    Added property to LeagueModel

--- GuessItSoccer.API/Models/LeagueModel.cs
+++ GuessItSoccer.API/Models/LeagueModel.cs
@@ -3,6 +3,7 @@
     public class LeagueModel
     {
         public long Id { get; set; }
+        public bool IsArchived { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
     }
